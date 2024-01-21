e8b5d873956c452bc3ba22a52ff5b721dc356377
emir245540 <157307747+emir245540@users.noreply.github.com>
2024-01-21 20:58:23 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+           "ROFL": "bir şakaya karşılık cevap",
+"SHEESH": "onaylamamak",
+"CREEPY": "korkunç",
+"AGGRO": "agresifleşmek/sinirlenmek" }
+            
+print(meme_dict ["AGGRO"])
